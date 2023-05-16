247fb3a91f1b64d34e77d4b31c314b893a7026c1
Adryel César <adryelcesarm@gmail.com>
2023-05-16 11:42:03 -0300

    atualiza projeto

--- src/database/config.ts
+++ src/database/config.ts
@@ -12,7 +12,7 @@ const config = () => {
         }
     }
 
-	const dburl = process.env.DATABASE_URL
+	const dbUrl = process.env.DATABASE_URL
 
     return {
         // user: process.env.DB_USER,
@@ -20,10 +20,41 @@ const config = () => {
         // host: process.env.DB_HOST,
         // database: process.env.DB,
         port: parseInt(process.env.DB_PORT!),
-		connectionString: dburl
+		connectionString: dbUrl
     }
 }
 
 const client: Client = new Client(config())
+
+export const createUserTableQuery = `
+CREATE TABLE IF NOT EXISTS users(
+	id SERIAL PRIMARY KEY,
+	username VARCHAR(20) NOT NULL,
+	email VARCHAR(45) NOT NULL UNIQUE,
+	"password" VARCHAR(120) NOT NULL,
+	"admin" BOOLEAN DEFAULT FALSE,
+	credits INTEGER DEFAULT 0,
+	active BOOLEAN DEFAULT TRUE
+)
+`
+
+export const { v4: uuidv4 } = require('uuid');
+
+export const createRastreioTableQuery = `
+CREATE TABLE IF NOT EXISTS rastreio(
+	id UUID DEFAULT '${uuidv4()}' PRIMARY KEY,
+	codigo VARCHAR(14) NOT NULL UNIQUE,
+	"data_de_postagem" VARCHAR(20) NOT NULL,
+	"descricao_inicial" VARCHAR(100) NOT NULL,
+	"endereco_envio" VARCHAR(100) NOT NULL,
+	"data_last_review" VARCHAR(20) NOT NULL,
+	"ultima_descricao" VARCHAR(100) NOT NULL,
+	"endereco_final" VARCHAR(100) NOT NULL,
+	"userId" INTEGER DEFAULT NULL,
+	FOREIGN KEY ("userId") REFERENCES users(id)
+)
+`
+
+
 
 export default client
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,8 +1,11 @@
 import client from './config'
+import { createUserTableQuery, createRastreioTableQuery, uuidv4 } from './config'
 
 const startDatabase = async (): Promise<void> => {
-    await client.connect()
-    console.log('Database started.')
+	await client.connect()
+	await client.query(createUserTableQuery)
+	await client.query(createRastreioTableQuery)
+	console.log('Database started.')
 }
 
 export default startDatabase
