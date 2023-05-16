81f43d6c3d61b6d0d717cbeeb0397512f86fe7ca
Adryel César <adryelcesarm@gmail.com>
2023-05-16 09:34:26 -0300

    atualiza connection

--- src/database/config.ts
+++ src/database/config.ts
@@ -12,12 +12,15 @@ const config = () => {
         }
     }
 
+	const dburl = process.env.DATABASE_URL
+
     return {
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        host: process.env.DB_HOST,
-        database: process.env.DB,
-        port: parseInt(process.env.DB_PORT!),
+        // user: process.env.DB_USER,
+        // password: process.env.DB_PASSWORD,
+        // host: process.env.DB_HOST,
+        // database: process.env.DB,
+        // port: parseInt(process.env.DB_PORT!),
+		connectionString: dburl
     }
 }
 
