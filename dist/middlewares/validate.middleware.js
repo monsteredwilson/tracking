646c454fdcaa00e16ff02b3cad8cbb581e8c1509
monsteredwilson <132711038+monsteredwilson@users.noreply.github.com>
2023-05-16 17:19:13 -0300

    Delete dist directory

--- dist/middlewares/validate.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateDataMiddleware = void 0;
-const validateDataMiddleware = (schema) => (request, response, next) => {
-    const validatedData = schema.parse(request.body);
-    request.body = validatedData;
-    return next();
-};
-exports.validateDataMiddleware = validateDataMiddleware;
