c90ead35a530ce8e34ebaf2032d6165ea2924c7f
Adryel César <adryelcesarm@gmail.com>
2023-05-17 18:37:13 -0300

    atualiza cors

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import cors from 'cors'
 
 const app: Application = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({origin: 'https://type-orm-deploy.onrender.com'}))
 app.use('/users', userRoutes)
 app.use('/login', loginRoutes)
 app.use('/codes', codesRoutes)
