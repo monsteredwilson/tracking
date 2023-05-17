6364a58b970f989044b0394b32bc10b93eeb0b62
Adryel César <adryelcesarm@gmail.com>
2023-05-17 17:42:41 -0300

    new count

--- src/services/codes/retrieveNumberRowsCodes.service.ts
+++ src/services/codes/retrieveNumberRowsCodes.service.ts
@@ -7,16 +7,16 @@ import { AppDataSource } from "../../data-source"
 
 export const retrieveNumberRowsCodesService = async (token: string | undefined, id: number): Promise<number> => {
 
-	const queryString: string = `
-	SELECT 
-		*
-	FROM 
-		rastreio
-	`
+	// const queryString: string = `
+	// SELECT 
+	// 	*
+	// FROM 
+	// 	rastreio
+	// `
 
 	const codesRepository: Repository<Rastreio> = AppDataSource.getRepository(Rastreio)
 
-	const queryResult = await codesRepository.query(queryString)
+	const queryResult = await codesRepository.count()
 
-	return queryResult.length
+	return queryResult
 }
