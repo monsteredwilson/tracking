4f68504702307dbc38b7cd548d40d76b79e97ce8
Adryel César <adryelcesarm@gmail.com>
2023-05-17 18:33:57 -0300

    install cors

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,11 @@ import { handleErrors } from './error'
 import { userRoutes } from './routes/users.routes'
 import { loginRoutes } from './routes/login.routes'
 import { codesRoutes } from './routes/codes.routes'
+import cors from 'cors'
 
 const app: Application = express()
 app.use(express.json())
+app.use(cors())
 app.use('/users', userRoutes)
 app.use('/login', loginRoutes)
 app.use('/codes', codesRoutes)
